0bcb4f039d291e4e3e12fad11f5ef234659f00d4
Chandru <chandru@mserv.in>
2018-09-21 12:59:02 +0530

    Updated the ezedox_pdfjs cdn paths. Cdn vesion set to 0.0.4

--- ezedox-constants.js
+++ ezedox-constants.js
@@ -1,9 +1,13 @@
+const CDN_VERSION = '0.0.4';
+
+const CDN_BASE_URL = `//cdn.jsdelivr.net/npm/ezedox_pdfjs@${CDN_VERSION}`;
+
 const EZEDOX_CONSTANTS = {
-    workerSrc: '//localhost:8888/build/generic/build/pdf.worker.js',
-    cMapUrl: '//localhost:8888/build/generic/web/cmaps/',
-    defaultUrl: '',
-    showPreviousViewOnLoad: false,
-    defaultZoomValue: 'auto',
+  workerSrc: `${CDN_BASE_URL}/build/generic/build/pdf.worker.js`,
+  cMapUrl: `${CDN_BASE_URL}/build/generic/web/cmaps/`,
+  defaultUrl: '',
+  showPreviousViewOnLoad: false,
+  defaultZoomValue: 'auto',
 };
 
 export default EZEDOX_CONSTANTS;
